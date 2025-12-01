8195f56e97c78290412e1b4dd9fd22a9219ce9ec
Chromilo Amin <chromiloamin@gmail.com>
2025-11-30 21:28:34 -0800

    Update content.tsx

--- src/resources/content.tsx
+++ src/resources/content.tsx
@@ -2,10 +2,10 @@ import { About, Blog, Gallery, Home, Newsletter, Person, Social, Work } from "@/
 import { Line, Row, Text } from "@once-ui-system/core";
 
 const person: Person = {
-  firstName: "Selene",
-  lastName: "Yu",
-  name: `Selene Yu`,
-  role: "Design Engineer",
+  firstName: "Chromilo",
+  lastName: "Amin",
+  name: `Chromilo Amin`,
+  role: "Enterprise Cloud Architect / Engineer",
   avatar: "/images/avatar.jpg",
   email: "example@gmail.com",
   location: "Asia/Jakarta", // Expecting the IANA time zone identifier, e.g., 'Europe/Vienna'
